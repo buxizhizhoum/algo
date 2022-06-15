b73d98b90879288aa91e8437c53e29b2a87acd95
Bytedance <mabinshan@bytedance.com>
2022-06-15 13:10:29 +0800

    141

---
+++ 141_linked_list_cycle/linked_list_cycle.go
@@ -0,0 +1,20 @@
+package linked_list_cycle
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	fast := head
+	slow := head
+
+	for ;fast != nil && fast.Next != nil; {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
