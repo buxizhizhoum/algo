b7405b4a26ee296bc872af17185f701d5b079f61
Bytedance <mabinshan@bytedance.com>
2022-06-19 11:31:29 +0800

    344

---
+++ 344_reverse_string/reverse_string.go
@@ -0,0 +1,12 @@
+package reverse_string
+
+func reverseString(s []byte)  {
+	left := 0
+	right := len(s) - 1
+	for ; left < right; {
+		s[left], s[right] = s[right], s[left]
+		left++
+		right--
+	}
+
+}
