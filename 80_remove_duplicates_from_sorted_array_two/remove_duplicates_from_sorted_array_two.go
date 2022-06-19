96a176f4050a0e31f147d6a8c0b867ce267c4f9a
Bytedance <mabinshan@bytedance.com>
2022-06-19 23:31:25 +0800

    80

--- 80_remove_duplicates_from_sorted_array_two/remove_duplicates_from_sorted_array_two.go
+++ 80_remove_duplicates_from_sorted_array_two/remove_duplicates_from_sorted_array_two.go
@@ -2,7 +2,21 @@ package main
 
 import "fmt"
 
-
+func removeDuplicates(nums []int) int {
+	if len(nums) < 2 {
+		return len(nums)
+	}
+	slow := 2
+	fast := 2
+	for ; fast < len(nums); {
+		if nums[slow - 2] != nums[fast] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+		fast++
+	}
+	return slow
+}
 
 
 func main() {
