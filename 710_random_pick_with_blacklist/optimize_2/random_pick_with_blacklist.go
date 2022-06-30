539209bc0dd0a9924cfcc53df9a93233f223a5b6
Bytedance <mabinshan@bytedance.com>
2022-06-30 15:03:49 +0800

    710 optimize

--- 710_random_pick_with_blacklist/optimize_2/random_pick_with_blacklist.go
+++ 710_random_pick_with_blacklist/optimize_2/random_pick_with_blacklist.go
@@ -29,14 +29,16 @@ func Constructor(n int, blacklist []int) Solution {
 		if val > n - len(blacklist) - 1 {
 			continue
 		}
-		for {
+		// todo: 注意这里的for循环写法
+		for _, rok := mapping[last];rok ; _, rok = mapping[last]{
 			// 找到末端元素不属于黑名单中的例子
-			_, rok := mapping[last]
-			if rok {
-				last--
-				continue
-			}
-			break
+			//_, rok := mapping[last]
+			//if rok {
+			//	last--
+			//	continue
+			//}
+			//break
+			last--
 		}
 		// 前半段的黑名单元素，映射到后段，val是[0, n)之间的一个数，代表了黑名单元素在前半段的索引
 		mapping[val] = last
@@ -75,7 +77,7 @@ func main() {
 	//blacklist := []int{2,3,5}
 	n:=4
 	//blacklist := []int{2,1}
-	blacklist := []int{0,1}
+	blacklist := []int{0,3}
 	//[1000000000,[640145908]
 	//n := 1000000000
 	//blacklist := []int{640145908}
