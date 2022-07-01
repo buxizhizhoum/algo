ed835ef5e6af670fe6f895adecbf8bd8f8be915c
Bytedance <mabinshan@bytedance.com>
2022-07-02 00:00:40 +0800

    543

--- 543_diameter_of_binary_tree/diameter_of_binary_tree.go
+++ 543_diameter_of_binary_tree/diameter_of_binary_tree.go
@@ -20,15 +20,15 @@ func diameter(root *TreeNode, res *int) int {
 		return 0
 	}
 	// 左右节点最大深度
-	leftDiameter := diameter(root.Left, res)
-	rightDiameter := diameter(root.Right, res)
+	leftDepth := diameter(root.Left, res)
+	rightDepth := diameter(root.Right, res)
 	// 当前节点的直径是左子树和右子树的和
 	// 最大直径是节点左子树最大深度 + 右子树之中最大深度
-	nodeDiameter := leftDiameter + rightDiameter
+	nodeDiameter := leftDepth + rightDepth
 	// 取最大的diameter
 	*res = max(*res, nodeDiameter)
 	// 返回节点最大深度
-	return 1 + max(leftDiameter, rightDiameter)
+	return 1 + max(leftDepth, rightDepth)
 }
 
 func max(a, b int) int {
