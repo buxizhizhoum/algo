f8f63fb58fa501402d62a224450ffd8b89f90546
Bytedance <mabinshan@bytedance.com>
2022-07-01 08:36:33 +0800

    104

---
+++ 104_maximum_depth_of_binary_tree/maximum_depth_of_binary_tree.go
@@ -0,0 +1,24 @@
+package maximum_depth_of_binary_tree
+
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftDepth := maxDepth(root.Left)
+	rightDepth := maxDepth(root.Right)
+	return max(leftDepth, rightDepth) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
