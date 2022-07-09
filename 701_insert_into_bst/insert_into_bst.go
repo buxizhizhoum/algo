a0a3b1ed336d03f8d8115928aacca07c2466aa7e
Bytedance <mabinshan@bytedance.com>
2022-07-09 22:50:05 +0800

    701

--- 701_insert_into_bst/insert_into_bst.go
+++ 701_insert_into_bst/insert_into_bst.go
@@ -1,4 +1,4 @@
-package insert_into_bst
+package main
 
 type TreeNode struct {
 	Val int
@@ -8,6 +8,8 @@ type TreeNode struct {
 
 func insertIntoBST(root *TreeNode, val int) *TreeNode {
 	return traverse(root, val)
+	// 在root == nil的时候，root是新建的，直接返回原来的root不行
+	// return root
 }
 
 func traverse(root *TreeNode, val int) *TreeNode {
@@ -20,9 +22,35 @@ func traverse(root *TreeNode, val int) *TreeNode {
 	}
 
 	if val < root.Val {
-		root.Left = insertIntoBST(root.Left, val)
+		root.Left = traverse(root.Left, val)
 	} else if val > root.Val {
-		root.Right = insertIntoBST(root.Right, val)
+		root.Right = traverse(root.Right, val)
 	}
 	return root
+}
+
+
+func main() {
+	root := &TreeNode{
+		Val:   4,
+		Left:  &TreeNode{
+			Val:   2,
+			Left:  &TreeNode{
+				Val:   1,
+				Left:  nil,
+				Right: nil,
+			},
+			Right: &TreeNode{
+				Val:   3,
+				Left:  nil,
+				Right: nil,
+			},
+		},
+		Right: &TreeNode{
+			Val:   7,
+			Left:  nil,
+			Right: nil,
+		},
+	}
+	insertIntoBST(root, 5)
 }
