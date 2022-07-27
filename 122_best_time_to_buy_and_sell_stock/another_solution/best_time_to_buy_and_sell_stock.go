5b0c003bdfb864a67c53abb16056048e0af9a560
Bytedance <mabinshan@bytedance.com>
2022-07-28 00:42:14 +0800

    714

--- 122_best_time_to_buy_and_sell_stock/another_solution/best_time_to_buy_and_sell_stock.go
+++ 122_best_time_to_buy_and_sell_stock/another_solution/best_time_to_buy_and_sell_stock.go
@@ -4,8 +4,10 @@ func maxProfit(prices []int) int {
 	dp := make([]int, len(prices))
 	dp[0] = 0
 	for i:=1;i<len(prices);i++{
-		// 昨天的最大利润，加上今天的最大利润
+		// 昨天的最大利润，或者昨天不卖，今天卖的最大利润
 		dp[i] = max(dp[i-1], dp[i-1] + prices[i] - prices[i-1])
+		// 昨天的最大利润，加上今天的最大利润
+		//dp[i] = dp[i-1] + max(0, prices[i] - prices[i-1])
 	}
 
 	return dp[len(prices) -1]
