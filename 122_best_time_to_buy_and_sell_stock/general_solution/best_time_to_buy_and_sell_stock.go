a27447568e51a695344d543cc4f9fef4410b9b52
Bytedance <mabinshan@bytedance.com>
2022-07-28 11:39:42 +0800

    122

--- 122_best_time_to_buy_and_sell_stock/general_solution/best_time_to_buy_and_sell_stock.go
+++ 122_best_time_to_buy_and_sell_stock/general_solution/best_time_to_buy_and_sell_stock.go
@@ -1,13 +1,16 @@
 package general_solution
 
 func maxProfit(prices []int) int {
+	// 不持有，第i天的收益
 	dp0 := make([]int, len(prices))
+	// 持有，第i天收益
 	dp1 := make([]int, len(prices))
 	dp0[0] = 0
 	dp1[0] = -prices[0]
 	for i:= 1;i<len(prices);i++ {
-		// 保持不动 or 前一天不卖，今天卖
+		// 保持不动 or 卖出
 		dp0[i] = max(dp0[i-1], dp1[i-1] + prices[i])
+		// 保持不动 or 买入
 		dp1[i] = max(dp1[i-1], dp0[i-1] - prices[i])
 	}
 	return dp0[len(prices) - 1]
