7d96b2ef26298287233594e05a720164997eb0d8
Bytedance <mabinshan@bytedance.com>
2022-08-01 17:46:54 +0800

    90

---
+++ 90_subsets/subsets.go
@@ -0,0 +1,29 @@
+package subsets
+
+import "sort"
+
+func subsetsWithDup(nums []int) [][]int {
+	res := make([][]int, 0)
+	track := make([]int, 0)
+	// 通过排序，并且在后面for循环中跳过重复元素避免重复
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
+	backtrack(nums, 0, track, &res)
+	return res
+}
+
+func backtrack(nums []int, start int, track []int, res *[][]int) {
+	tmp := make([]int, len(track))
+	copy(tmp, track)
+	*res = append(*res, tmp)
+
+	for i:= start;i<len(nums);i++{
+		if i > start && nums[i] == nums[i-1] {
+			continue
+		}
+		track = append(track, nums[i])
+		backtrack(nums, i+1, track, res)
+		track = track[:len(track) - 1]
+	}
+}
