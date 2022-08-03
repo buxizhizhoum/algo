0423264f2b3360a8d0af672aae2eb333c3da2499
Bytedance <mabinshan@bytedance.com>
2022-08-03 21:43:16 +0800

    1020

---
+++ 1020_number_of_enclaves/another_solution/number_of_enclaves.go
@@ -0,0 +1,54 @@
+package another_solution
+
+func numEnclaves(grid [][]int) int {
+	m := len(grid)
+	n := len(grid[0])
+	visited := make([][]int, m)
+	for i:=0;i < m;i++ {
+		visited[i] = make([]int, n)
+	}
+	directions := [][]int{
+		{-1, 0},
+		{0, 1},
+		{1, 0},
+		{0, -1},
+	}
+	res := 0
+	// 把靠边的都标记为访问，然后直接数飞地数量即可
+	for i:=0 ;i<m;i++{
+		dfs(grid, visited, directions, i, 0, m, n, &res)
+		dfs(grid, visited, directions, i, n-1, m, n, &res)
+	}
+	for j:=0;j<n;j++{
+		dfs(grid, visited, directions, 0, j, m, n, &res)
+		dfs(grid, visited, directions, m-1, j, m, n, &res)
+	}
+	res = 0
+	for i:=1;i<m;i++ {
+		for j:=1;j<n;j++{
+			if grid[i][j] == 1 && visited[i][j] == 0 {
+				res += 1
+			}
+		}
+	}
+	return res
+}
+
+func dfs(grid [][]int, visited [][]int, directions[][]int, i, j, m, n int, res *int) {
+	if i<0||j <0||i>=m||j>=n {
+		return
+	}
+	if visited[i][j] == 1 {
+		return
+	}
+	if grid[i][j] == 0 {
+		visited[i][j] = 1
+		return
+	}
+	visited[i][j] = 1
+	*res += 1
+	for _, dir := range directions {
+		dfs(grid, visited, directions, i+dir[0], j+dir[1], m, n, res)
+	}
+	return
+}
