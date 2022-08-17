3ffbaaaf77b2f58f3fea9fae662e0b71adeab8fc
Bytedance <mabinshan@bytedance.com>
2022-08-17 18:00:34 +0800

    print 0 1

---
+++ print_0_1/print_0_1.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup = sync.WaitGroup{}
+
+func printf(ch chan int) {
+	for i:=0;i<10;i++ {
+		num := <- ch
+		fmt.Println(num)
+		ch <- (num + 1) % 2
+	}
+	wg.Done()
+}
+
+func main() {
+
+	ch := make(chan int, 1)
+	ch <- 0
+	wg.Add(2)
+	go printf(ch)
+	go printf(ch)
+	wg.Wait()
+}
