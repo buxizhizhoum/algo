88b0d428553d5f100ed02e3cddf82f844ec7c7a1
Bytedance <mabinshan@bytedance.com>
2022-09-02 16:38:20 +0800

    40

---
+++ 40_combination_sum_two/combination_sum_two.go
@@ -0,0 +1,44 @@
+package combination_sum_two
+
+import "sort"
+
+func combinationSum2(candidates []int, target int) [][]int {
+	track := make([]int, 0)
+	res := make([][]int, 0)
+	sort.Slice(candidates, func(i, j int) bool {
+		return candidates[i] < candidates[j]
+	})
+	backtrack(candidates, track, target, 0, &res)
+	return res
+}
+
+func backtrack(candidates []int, track []int, target int, startIndex int, res *[][]int) {
+	if target == 0 {
+		//tmp := copy(track)
+		tmp := make([]int, len(track))
+		copy(tmp, track)
+		*res = append(*res, tmp)
+	}
+	if target < 0 {
+		return
+	}
+
+	for i:= startIndex; i < len(candidates); i++ {
+		// 树层去重，重复的元素不再选取
+		if i > startIndex && candidates[i] == candidates[i-1] {
+			continue
+		}
+		track = append(track, candidates[i])
+		// 元素不能重复选，这里startIndex = i + 1
+		backtrack(candidates, track, target - candidates[i], i + 1, res)
+		track = track[:len(track) - 1]
+	}
+}
+
+//func copy(track []int) []int {
+//	res := make([]int, len(track))
+//	for i, v := range track {
+//		res[i] = v
+//	}
+//	return res
+//}
