b3488de75cc4fce597a69869e78569bcc9b69bd2
Bytedance <mabinshan@bytedance.com>
2022-09-03 23:16:48 +0800

    90

---
+++ 90_subsets/solution_with_map/subsets.go
@@ -0,0 +1,32 @@
+package subsets
+
+import "sort"
+
+func subsetsWithDup(nums []int) [][]int {
+	res := make([][]int, 0)
+	track := make([]int, 0)
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
+	backtrack(nums, track, &res, 0)
+	return res
+}
+
+func backtrack(nums []int, track []int, res *[][]int, startIndex int) {
+	tmp := make([]int, len(track))
+	copy(tmp, track)
+	*res = append(*res, tmp)
+
+	// 使用used数组进行去重
+	used := make(map[int]int, 0)
+	for i := startIndex ; i< len(nums); i++ {
+		if count, _ := used[nums[i]]; count > 0 {
+			continue
+		}
+
+		used[nums[i]]++
+		track = append(track, nums[i])
+		backtrack(nums, track, res, i + 1)
+		track = track[:len(track) - 1]
+	}
+}
