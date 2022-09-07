b92bc639fa3415f5b71fa0f6fc634d7a88433656
Bytedance <mabinshan@bytedance.com>
2022-09-07 15:50:54 +0800

    746

---
+++ 746_min_cost_climbing_stairs/min_cost_climb_stairs.go
@@ -0,0 +1,18 @@
+package min_cost_climbing_stairs
+
+func minCostClimbingStairs(cost []int) int {
+	dp := make([]int, len(cost))
+	dp[0] = cost[0]
+	dp[1] = cost[1]
+	for i:= 2;i<len(cost);i++ {
+		dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
+	}
+	return min(dp[len(dp)-1], dp[len(dp) - 2])
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
